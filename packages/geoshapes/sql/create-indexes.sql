ac54f3ee4522f003597b2f888f1ece49ce63927e
mbsantos <msantos@wikimedia.org>
2021-08-17 01:18:36 +0200

    geoshapes: fix query with new imposm mapping
    and bump geoshapes to 1.1.5
    
    Change-Id: I1dc1e311cdefdb6f72540c4b678f8db7767a2c95

--- packages/geoshapes/sql/create-indexes.sql
+++ packages/geoshapes/sql/create-indexes.sql
@@ -43,3 +43,45 @@ IF NOT EXISTS (
 END IF;
 
 END$$;
+
+--
+-- Name: wikidata_relation_members_idx; Type: INDEX; Schema: public; Owner: osmimporter; Tablespace:
+--
+
+DO $$
+BEGIN
+
+IF NOT EXISTS (
+    SELECT 1
+    FROM   pg_class c
+    JOIN   pg_namespace n ON n.oid = c.relnamespace
+    WHERE  c.relname = 'wikidata_relation_members_idx'
+    AND    n.nspname = 'public'
+    ) THEN
+
+    CREATE INDEX wikidata_relation_members_idx ON wikidata_relation_members (wikidata);
+
+END IF;
+
+END$$;
+
+--
+-- Name: wikidata_relation_polygon_idx; Type: INDEX; Schema: public; Owner: osmimporter; Tablespace:
+--
+
+DO $$
+BEGIN
+
+IF NOT EXISTS (
+    SELECT 1
+    FROM   pg_class c
+    JOIN   pg_namespace n ON n.oid = c.relnamespace
+    WHERE  c.relname = 'wikidata_relation_polygon_idx'
+    AND    n.nspname = 'public'
+    ) THEN
+
+    CREATE INDEX wikidata_relation_polygon_idx ON wikidata_relation_polygon (wikidata);
+
+END IF;
+
+END$$;
