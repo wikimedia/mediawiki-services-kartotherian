1026c089f836de399913c69b97f7ed3b9ba3290a
WMDE-Fisch <christoph.jauera@wikimedia.de>
2022-02-25 14:01:04 +0100

    Lint fixups in input-validator
    Let's keep the leftover for a follow up once we're mono. Easier
    to handle then.
    
    Change-Id: I85b6153f1f9e995e1da51783b0a181472b644cb6

--- packages/input-validator/validator.js
+++ packages/input-validator/validator.js
@@ -1,158 +1,178 @@
-'use strict';
+// FIXME: Satisfy these lint rules
+/* eslint-disable no-param-reassign, no-multi-assign */
 
-let _ = require('underscore'),
-    qidx = require('quadtile-index'),
-    qs = require('querystring'),
-    urllib = require('url'),
-    Err = require('@wikimedia/err');
-
-module.exports = checkType;
+const _ = require('underscore');
+const qidx = require('quadtile-index');
+const qs = require('querystring');
+const urllib = require('url');
+const Err = require('@wikimedia/err');
 
 function getDefault(obj, field, mustHave) {
-    if (mustHave === true) {
-        throw new Err('Value %j is missing', field);
-    }
-    if (mustHave === false || mustHave === undefined) {
-        delete obj[field];
-    } else {
-        obj[field] = mustHave;
-    }
-    return false;
+  if (mustHave === true) {
+    throw new Err('Value %j is missing', field);
+  }
+  if (mustHave === false || mustHave === undefined) {
+    delete obj[field];
+  } else {
+    obj[field] = mustHave;
+  }
+  return false;
 }
 
 /**
  * Utility method to check the type of the object's property
  */
 function checkType(obj, field, expType, mustHave, min, max) {
-    let value = typeof (obj) === 'object' ? obj[field] : obj;
-    if (value === undefined) {
-        return getDefault(obj, field, mustHave);
-    }
-    // Try to convert value to expected type
-    let type = expType[0] === '[' ? Object.prototype.toString.call(value) : typeof value;
-    if (type === 'string') {
-        switch (expType) {
-            case 'number':
-                value = checkType.strToFloat(value);
-                type = typeof value;
-                break;
-            case 'integer':
-            case 'zoom':
-                obj[field] = value = checkType.strToInt(value);
-                type = typeof value;
-                break;
-            case 'boolean':
-                obj[field] = value = !!value;
-                type = typeof value;
-                break;
-            case 'string-array':
-                obj[field] = value = [value];
-                type = typeof value;
-                break;
-            case 'number-array':
-                value = checkType.strToFloat(value);
-                type = typeof value;
-                if (type === 'number') {
-                    obj[field] = value = [value];
-                }
-                break;
-        }
-    } else if (type === 'number' && expType === 'number-array') {
+  let value = typeof (obj) === 'object' ? obj[field] : obj;
+  if (value === undefined) {
+    return getDefault(obj, field, mustHave);
+  }
+  // Try to convert value to expected type
+  let type = expType[0] === '[' ? Object.prototype.toString.call(value) : typeof value;
+  if (type === 'string') {
+    switch (expType) {
+      case 'number':
+        value = checkType.strToFloat(value);
+        type = typeof value;
+        break;
+      case 'integer':
+      case 'zoom':
+        obj[field] = value = checkType.strToInt(value);
+        type = typeof value;
+        break;
+      case 'boolean':
+        obj[field] = value = !!value;
+        type = typeof value;
+        break;
+      case 'string-array':
         obj[field] = value = [value];
         type = typeof value;
+        break;
+      case 'number-array':
+        value = checkType.strToFloat(value);
+        type = typeof value;
+        if (type === 'number') {
+          obj[field] = value = [value];
+        }
+        break;
+      default:
+        break;
     }
+  } else if (type === 'number' && expType === 'number-array') {
+    obj[field] = value = [value];
+    type = typeof value;
+  }
 
-    // validate the type
-    switch (expType) {
-        case 'string-array':
-            if (!Array.isArray(value) ||
+  // validate the type
+  let isValid;
+  switch (expType) {
+    case 'string-array':
+      if (!Array.isArray(value) ||
                 !_.all(value, v => typeof v === 'string' && v.length > 0)
-            ) {
-                throw new Err('Invalid %s param: expecting a string or an array of strings', field);
-            }
-            break;
-        case 'number-array':
-            let isValid = Array.isArray(value);
-            if (isValid) {
-                value = _.map(value, v => {
-                    v = checkType.strToFloat(v);
-                    if (typeof v !== 'number') {
-                        isValid = false;
-                    }
-                    return v;
-                });
-            }
-            if (!isValid) {
-                throw new Err('Invalid %s param: expecting a number or an array of numbers', field);
-            }
-            obj[field] = value;
-            break;
-        case 'array':
-            if (!Array.isArray(value)) {
-                throw new Err('Invalid %s param type %s given, was expecting an array',
-                    field, type);
-            }
-            break;
-        case 'integer':
-            if (!Number.isInteger(value)) {
-                throw new Err('Invalid %s param type %s given, was expecting an integer',
-                    field, type);
-            }
-            break;
-        case 'zoom':
-            if (!qidx.isValidZoom(value)) {
-                throw new Err('Invalid %s param - an integer zoom value was expected', field);
-            }
-            break;
-        default:
-            if (type !== expType) {
-                throw new Err('Invalid %s param type %s given, was expecting %s',
-                    field, type, expType);
-            }
-            break;
-    }
+      ) {
+        throw new Err('Invalid %s param: expecting a string or an array of strings', field);
+      }
+      break;
+    case 'number-array':
+      isValid = Array.isArray(value);
+      if (isValid) {
+        value = _.map(value, (v) => {
+          v = checkType.strToFloat(v);
+          if (typeof v !== 'number') {
+            isValid = false;
+          }
+          return v;
+        });
+      }
+      if (!isValid) {
+        throw new Err('Invalid %s param: expecting a number or an array of numbers', field);
+      }
+      obj[field] = value;
+      break;
+    case 'array':
+      if (!Array.isArray(value)) {
+        throw new Err(
+          'Invalid %s param type %s given, was expecting an array',
+          field, type
+        );
+      }
+      break;
+    case 'integer':
+      if (!Number.isInteger(value)) {
+        throw new Err(
+          'Invalid %s param type %s given, was expecting an integer',
+          field, type
+        );
+      }
+      break;
+    case 'zoom':
+      if (!qidx.isValidZoom(value)) {
+        throw new Err('Invalid %s param - an integer zoom value was expected', field);
+      }
+      break;
+    default:
+      if (type !== expType) {
+        throw new Err(
+          'Invalid %s param type %s given, was expecting %s',
+          field, type, expType
+        );
+      }
+      break;
+  }
 
-    // validate ranges
-    switch (expType) {
-        case 'number':
-        case 'integer':
-        case 'zoom':
-            if (min !== undefined && value < min) {
-                throw new Err('Invalid %s param - must be at least %d, but given %d',
-                    field, min, value);
-            }
-            if (max !== undefined && value > max) {
-                throw new Err('Invalid %s param - must be at most %d, but given %d',
-                    field, max, value);
-            }
-            break;
-        case 'string':
-            if (min !== undefined && value.length < min) {
-                throw new Err('Invalid %s param - the string must be at least %d symbols',
-                    field, min);
-            }
-            break;
-        case 'boolean':
-            if (value === false) {
-                // convert false into undefined
-                delete obj[field];
-                return false;
-            }
-            break;
-        case 'string-array':
-        case 'number-array':
-            if (min !== undefined && value.length < min) {
-                throw new Err('Invalid %s param - it must have at least %d values, but given %d',
-                    field, min, value.length);
-            }
-            if (max !== undefined && value.length > max) {
-                throw new Err('Invalid %s param - it must have at least %d values, but given %d',
-                    field, max, value.length);
-            }
-            break;
-    }
+  // validate ranges
+  switch (expType) {
+    case 'number':
+    case 'integer':
+    case 'zoom':
+      if (min !== undefined && value < min) {
+        throw new Err(
+          'Invalid %s param - must be at least %d, but given %d',
+          field, min, value
+        );
+      }
+      if (max !== undefined && value > max) {
+        throw new Err(
+          'Invalid %s param - must be at most %d, but given %d',
+          field, max, value
+        );
+      }
+      break;
+    case 'string':
+      if (min !== undefined && value.length < min) {
+        throw new Err(
+          'Invalid %s param - the string must be at least %d symbols',
+          field, min
+        );
+      }
+      break;
+    case 'boolean':
+      if (value === false) {
+        // convert false into undefined
+        delete obj[field];
+        return false;
+      }
+      break;
+    case 'string-array':
+    case 'number-array':
+      if (min !== undefined && value.length < min) {
+        throw new Err(
+          'Invalid %s param - it must have at least %d values, but given %d',
+          field, min, value.length
+        );
+      }
+      if (max !== undefined && value.length > max) {
+        throw new Err(
+          'Invalid %s param - it must have at least %d values, but given %d',
+          field, max, value.length
+        );
+      }
+      break;
+    default:
+      break;
+  }
 
-    return true;
+  return true;
 }
 
 /**
@@ -165,10 +185,10 @@ checkType.floatRe = /^-?\d+(?:[.,]\d*?)?$/;
  * Converts value to float if possible, or returns the original
  */
 checkType.strToFloat = function strToFloat(value) {
-    if (typeof value === 'string' && checkType.floatRe.test(value)) {
-        return parseFloat(value);
-    }
-    return value;
+  if (typeof value === 'string' && checkType.floatRe.test(value)) {
+    return parseFloat(value);
+  }
+  return value;
 };
 
 
@@ -182,10 +202,10 @@ const intRe = /^-?\d+$/;
  * Converts value to integer if possible, or returns the original
  */
 checkType.strToInt = function strToInt(value) {
-    if (typeof value === 'string' && intRe.test(value)) {
-        return parseInt(value);
-    }
-    return value;
+  if (typeof value === 'string' && intRe.test(value)) {
+    return parseInt(value, 10);
+  }
+  return value;
 };
 
 /**
@@ -194,18 +214,20 @@ checkType.strToInt = function strToInt(value) {
  * @returns {*}
  */
 checkType.normalizeUrl = function normalizeUrl(uri) {
-    if (typeof uri === 'string') {
-        uri = urllib.parse(uri, true);
-    } else if (typeof uri.query === 'string') {
-        uri.query = qs.parse(uri.query);
-    }
-    uri.query = uri.query || {};
+  if (typeof uri === 'string') {
+    uri = urllib.parse(uri, true);
+  } else if (typeof uri.query === 'string') {
+    uri.query = qs.parse(uri.query);
+  }
+  uri.query = uri.query || {};
 
-    /**
+  /**
      * Allow info property to be injected into the URI objects to make tilelive-http [1]
      * accept maxzoom and minzoom properties - T297753
      * [1] https://github.com/mojodna/tilelive-http/blob/master/index.js#L133
      */
-    uri.info = uri.info || {};
-    return uri;
+  uri.info = uri.info || {};
+  return uri;
 };
+
+module.exports = checkType;
