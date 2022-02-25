96d743e373243b38c8f35bbef4c90238d160e7bd
WMDE-Fisch <christoph.jauera@wikimedia.de>
2022-02-25 17:38:38 +0100

    Lint fixups in err
    Change-Id: I7abe7448f382ae40fad9b51e698efc457c0a3d93

--- packages/err/Err.js
+++ packages/err/Err.js
@@ -1,6 +1,4 @@
-'use strict';
-
-let util = require('util');
+const util = require('util');
 
 /**
  * Creates a formatted error info
@@ -8,19 +6,19 @@ let util = require('util');
  * @returns {Err}
  * @constructor
  */
-function Err(message) {
-    Error.captureStackTrace(this, this.constructor);
-    this.name = this.constructor.name;
-    this.message = arguments.length < 2
-        ? (message || 'unknown')
-        : util.format.apply(null, arguments);
+function Err(...messageParams) {
+  Error.captureStackTrace(this, this.constructor);
+  this.name = this.constructor.name;
+  this.message = messageParams.length < 2
+    ? (messageParams[0] || 'unknown')
+    : util.format.apply(null, messageParams);
 }
 
 util.inherits(Err, Error);
 
-Err.prototype.metrics = function(metrics) {
-    this.metrics = metrics;
-    return this;
+Err.prototype.metrics = function metricsFunc(metrics) {
+  this.metrics = metrics;
+  return this;
 };
 
 /**
@@ -28,14 +26,14 @@ Err.prototype.metrics = function(metrics) {
  * The error message string is often used to check if tile existance, so it has to be exact
  */
 Err.throwNoTile = function throwNoTile() {
-    throw new Error('Tile does not exist');
+  throw new Error('Tile does not exist');
 };
 
 /**
  * Checks if the error indicates the tile does not exist
  */
 Err.isNoTileError = function isNoTileError(err) {
-    return err.message === 'Tile does not exist';
+  return err.message === 'Tile does not exist';
 };
 
 
