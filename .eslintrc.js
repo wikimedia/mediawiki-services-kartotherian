eed7e2aab9b9d75f7c85d211b6c45f40604eff2d
WMDE-Fisch <christoph.jauera@wikimedia.de>
2022-03-15 18:53:28 +0100

    Minor cleanup in .eslintrc files
    - jest only used in test/
    - use "off"
    - narrow statements per line
    
    Change-Id: I2ebdd3ebe0b054882f8cbb06ef27a8844dbf99eb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,14 @@
 module.exports = {
   root: true,
   extends: 'wikimedia/server',
-  env: {
-    jest: true,
-  },
   parserOptions: {
     ecmaVersion: 2018,
   },
   rules: {
-    'valid-jsdoc': [0],
-    'no-underscore-dangle': [0],
-    'max-statements-per-line': [0],
-    'camelcase': [0],
+    'valid-jsdoc': 'off',
+    'no-underscore-dangle': 'off',
+    'max-statements-per-line': [ 'error', { 'max': 2 } ],
+    'camelcase': 'off',
 
     // Dangling commas are not supported for functions in node before v8.x
     'comma-dangle': [
--- static/.eslintrc.js
+++ static/.eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  ignorePatterns: ["lib/*"],
-  extends: "wikimedia/client",
+  root: true,
+  ignorePatterns: [ 'lib/*' ],
+  extends: 'wikimedia/client',
   env: {
     node: false,
   },
   globals: {
     L: false
   },
-  root: true
 };
