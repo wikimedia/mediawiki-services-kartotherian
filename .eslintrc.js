22e0fae7492393201d1fd93000e336716b2a5c5a
WMDE-Fisch <christoph.jauera@wikimedia.de>
2022-03-22 13:48:30 +0100

    Bump eslint to 7.32.0
    Further to current wikimedia lint rules. Includes lint auto fixes.
    
    Change-Id: I05167be644c8910f0c463720707c4f36741aacb1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-  root: true,
-  extends: 'wikimedia/server',
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-  rules: {
-    'valid-jsdoc': 'off',
-    'no-underscore-dangle': 'off',
-    'max-statements-per-line': [ 'error', { 'max': 2 } ],
-    'camelcase': 'off',
+	root: true,
+	extends: 'wikimedia/server',
+	parserOptions: {
+		ecmaVersion: 2018,
+	},
+	rules: {
+		'valid-jsdoc': 'off',
+		'no-underscore-dangle': 'off',
+		'max-statements-per-line': [ 'error', { max: 2 } ],
+		camelcase: 'off',
 
-    // Dangling commas are not supported for functions in node before v8.x
-    'comma-dangle': [
-      'error',
-      {
-        arrays: 'always-multiline',
-        objects: 'always-multiline',
-        imports: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'never',
-      },
-    ],
-  },
+		// Dangling commas are not supported for functions in node before v8.x
+		'comma-dangle': [
+			'error',
+			{
+				arrays: 'always-multiline',
+				objects: 'always-multiline',
+				imports: 'always-multiline',
+				exports: 'always-multiline',
+				functions: 'never',
+			},
+		],
+	},
 };
--- static/.eslintrc.js
+++ static/.eslintrc.js
@@ -1,11 +1,12 @@
+/* eslint-env node, es6 */
 module.exports = {
-  root: true,
-  ignorePatterns: [ 'lib/*' ],
-  extends: 'wikimedia/client',
-  env: {
-    node: false,
-  },
-  globals: {
-    L: false
-  },
+	root: true,
+	ignorePatterns: [ 'lib/*' ],
+	extends: 'wikimedia/client',
+	env: {
+		node: false
+	},
+	globals: {
+		L: false
+	}
 };
