7d391655e27bceea3547cf30f8cd10be933abc2a
WMDE-Fisch <christoph.jauera@wikimedia.de>
2023-02-13 16:47:29 +0100

    Autoposition: Fallback to zero lat lon defaults and minzoom
    When data could not be retreived or autopositioning fails fall back
    to a baseline.
    
    Bug: T323377
    Change-Id: I46f97217fed23bc0457fa3f611982aba2bc1d382

--- test/snapshot/feature/autoPosition.test.js
+++ test/snapshot/feature/autoPosition.test.js
@@ -7,6 +7,10 @@
 const autoPosition = require( '../../../lib/snapshot/autoPosition' );
 const geoJsonPolygon = require( '../fixtures/autoposition-geojson-polygon.json' );
 const geoJsonPoint = require( '../fixtures/autoposition-geojson-point.json' );
+const geoJsonEmpty = {
+	type: 'FeatureCollection',
+	features: []
+};
 
 describe( 'autoposition', () => {
 	test( 'lat, lon and zoom set as auto', async () => {
@@ -20,7 +24,8 @@ describe( 'autoposition', () => {
 		expect( coordinates ).toEqual( {
 			latitude: -23.17508256414426,
 			longitude: -45.85693359375,
-			zoom: 8
+			zoom: 8,
+			success: true
 		} );
 	} );
 	describe( 'when param is set for lat/lon/zoom it should return the param values', () => {
@@ -35,7 +40,8 @@ describe( 'autoposition', () => {
 			expect( coordinates ).toEqual( {
 				latitude: -23.17508256414426,
 				longitude: -45.85693359375,
-				zoom: 7
+				zoom: 7,
+				success: true
 			} );
 		} );
 
@@ -50,7 +56,8 @@ describe( 'autoposition', () => {
 			expect( coordinates ).toEqual( {
 				latitude: -23.17508256414426,
 				longitude: -45,
-				zoom: 8
+				zoom: 8,
+				success: true
 			} );
 		} );
 
@@ -65,7 +72,8 @@ describe( 'autoposition', () => {
 			expect( coordinates ).toEqual( {
 				latitude: -23,
 				longitude: -45.85693359375,
-				zoom: 8
+				zoom: 8,
+				success: true
 			} );
 		} );
 
@@ -80,7 +88,8 @@ describe( 'autoposition', () => {
 			expect( coordinates ).toEqual( {
 				latitude: 0,
 				longitude: 0,
-				zoom: 20
+				zoom: 20,
+				success: true
 			} );
 		} );
 
@@ -95,7 +104,40 @@ describe( 'autoposition', () => {
 			expect( coordinates ).toEqual( {
 				latitude: 0,
 				longitude: 0,
-				zoom: 19
+				zoom: 19,
+				success: true
+			} );
+		} );
+
+		test( 'for empty data partly fallback to defaults', async () => {
+			const coordinates = autoPosition( {
+				lat: 'a',
+				lon: 2,
+				zoom: 13,
+				w: 100,
+				h: 100
+			}, geoJsonEmpty, 1, 19 );
+			expect( coordinates ).toEqual( {
+				latitude: 0,
+				longitude: 2,
+				zoom: 13,
+				success: false
+			} );
+		} );
+
+		test( 'for empty data fully fallback to defaults', async () => {
+			const coordinates = autoPosition( {
+				lat: 'a',
+				lon: 'a',
+				zoom: 'a',
+				w: 100,
+				h: 100
+			}, geoJsonEmpty, 1, 19 );
+			expect( coordinates ).toEqual( {
+				latitude: 0,
+				longitude: 0,
+				zoom: 1,
+				success: false
 			} );
 		} );
 	} );
