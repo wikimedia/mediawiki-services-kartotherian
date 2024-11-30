6f21612eabd74477aa49a850abc87d6b34de3cd6
Luca Toscano <ltoscano@wikimedia.org>
2024-11-30 11:10:51 +0100

    Remove mapnik build step in favor of a base image
    Remove the mapnik build step in favor of the same done in a base
    (build) image, that is re-used every time a change is filed
    for Kartotherian (more efficient for devs, it cuts the wait time
    from minutes to less than one).
    
    The final test/production images are also changed, to use runtime
    dependency libs rather than -dev ones (better in terms of security
    exposure and space used on disk).
    
    Bug: T327396
    Depends-On: https://gerrit.wikimedia.org/r/c/operations/docker-images/production-images/+/1100070
    Change-Id: I9c5cab492123e84b59f81d4c304ea11a4de95f78

--- scripts/install-mapnik.sh
+++ scripts/install-mapnik.sh
@@ -4,6 +4,3 @@ set -ex
 
 cd /srv/app
 /usr/bin/git submodule update --init --recursive
-cd vendor/mapnik
-/usr/bin/python3 ./scons/scons.py configure PREFIX=/opt/lib/mapnik
-/usr/bin/python3 ./scons/scons.py install -j8
